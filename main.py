2a9aa76817434008aab2359bbc8ee772d202ce2b
Gautham <gautham@Gauthams-MacBook-Pro.local>
2025-07-02 13:05:56 -0700

    added files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print("Welcome to the Tip Calculator")
-order_total = float(input("What's the total amount of your order? "))
-tip_percent = float(input("What percent do want to tip?"))
+order_total = float(input("What's the total amount of your order? $"))
+tip_percent = float(input("What percent do you want to tip? ")) / 100
 
-tip_amount = float(order_total*tip_percent)
-final_order_total = order_total+tip_amount
+tip_amount = order_total * tip_percent
+final_order_total = order_total + tip_amount
 
-print(tip_amount)
-print(final_order_total)
+print(f"Tip amount: ${tip_amount:.2f}")
+print(f"Total amount to pay: ${final_order_total:.2f}")
